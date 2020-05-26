31845f780efe3474e18ab5a494426b5bb0c5ed2d
Diana <DianaMaer@users.noreply.github.com>
2020-05-26 17:10:08 +0200

    solve cutting first, last

--- exercises/cut.js
+++ exercises/cut.js
@@ -11,9 +11,33 @@
  */
 
 // Your code:
+function cutFirst(word){
+    return word.slice(2);
+}
+function cutLast(word){
+    return word.slice(0 ,word.length-2);
+}
+
+function cutFirstLast(word){
+    return word.slice(2 ,word.length-2);
+}
 
 //* Begin of tests
 const assert = require('assert');
+assert.strictEqual(typeof cutFirst, 'function' );
+assert.strictEqual(cutFirst.length, 1);
+assert.strictEqual(cutFirst('Hello'), 'llo');
+assert.strictEqual(cutFirst('Hi people'), ' people');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof cutLast, 'function' );
+assert.strictEqual(cutLast.length, 1);
+assert.strictEqual(cutLast('Hello'), 'Hel');
+assert.strictEqual(cutLast('Hi people'), 'Hi peop');
+
+assert.strictEqual(typeof cutFirstLast, 'function' );
+assert.strictEqual(cutFirstLast.length, 1);
+assert.strictEqual(cutFirstLast('Hello'), 'l');
+assert.strictEqual(cutFirstLast('Hi people'), ' peop');
+
+// assert.fail('You must write your own tests');
 // End of tests */
