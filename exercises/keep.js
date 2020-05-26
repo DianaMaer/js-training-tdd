0c36bfacc57bf657757a1eb259ec96c3a4387592
Diana <DianaMaer@users.noreply.github.com>
2020-05-26 17:20:17 +0200

    solve kepping 2 characters

--- exercises/keep.js
+++ exercises/keep.js
@@ -13,9 +13,33 @@
  */
 
 // Your code:
+function keepFirst(word){
+    return word.substr(0,2);
+}
+function keepLast(word){
+    return word.substr(word.length-2,2);
+}
+
+function keepFirstLast(word){
+    return word.substr(2,2);
+}
 
 //* Begin of tests
 const assert = require('assert');
+assert.strictEqual(typeof keepFirst, 'function' );
+assert.strictEqual(keepFirst.length, 1);
+assert.strictEqual(keepFirst('Hello'), 'He');
+assert.strictEqual(keepFirst('H people'), 'H ');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof keepLast, 'function' );
+assert.strictEqual(keepLast.length, 1);
+assert.strictEqual(keepLast('Hello'), 'lo');
+assert.strictEqual(keepLast('Mini m'), ' m');
+
+assert.strictEqual(typeof keepFirstLast, 'function' );
+assert.strictEqual(keepFirstLast.length, 1);
+assert.strictEqual(keepFirstLast('Hello'), 'll');
+assert.strictEqual(keepFirstLast('Hi people'), ' p');
+
+//assert.fail('You must write your own tests');
 // End of tests */
